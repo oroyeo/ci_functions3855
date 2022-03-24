0e15286e36a02ee0622ec9e9a1150a600ba43e1c
Roy Ortega <55458208+oroyeo@users.noreply.github.com>
2022-03-23 17:56:10 -0700

    Create python_build.groovy

---
+++ vars/python_build.groovy
@@ -0,0 +1,37 @@
+def call(dockerRepoName, imageName) {
+    pipeline { 
+        agent any  
+        stages { 
+            stage('Build') { 
+                steps { 
+                    sh 'pip install -r requirements.txt' 
+                } 
+            } 
+            stage('Python Lint') { 
+                steps { 
+                    sh 'pylint-fail-under --fail_under 5.0 *.py' 
+                } 
+            }
+            stage('Zip Artifacts') {
+                steps {
+                    script {
+                        zip archive: true, dir: '', glob: '*.py', zipFile: 'app.zip', overwrite: true
+                    }
+                    archiveArtifacts artifacts: 'app.zip', fingerprint: true
+            }
+        }
+            stage('Package') { 
+                when { 
+                    expression { env.GIT_BRANCH == 'origin/main' } 
+                } 
+                steps { 
+                    withCredentials([string(credentialsId: 'DockerHub', variable: 'TOKEN')]) { 
+                    sh "docker login -u 'rortega4' -p '$TOKEN' docker.io" 
+                    sh "docker build -t ${dockerRepoName}:latest --tag rortega4/${dockerRepoName}:${imageName} ." 
+                    sh "docker push rortega4/${dockerRepoName}:${imageName}" 
+                } 
+            } 
+        } 
+    }
+}
+}
